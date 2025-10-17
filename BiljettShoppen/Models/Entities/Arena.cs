13a198af0c45086a0a9cc6e22d6d5bb5eb752453
h24tober <h24tober@du.se>
2025-10-17 10:02:30 +0200

    Class Arena Done!

--- BiljettShoppen/Models/Entities/Arena.cs
+++ BiljettShoppen/Models/Entities/Arena.cs
@@ -4,5 +4,9 @@ namespace Models.Entities;
 
 public class Arena : BaseEntity
 {
-    
+    public string Address { get; set; }
+    public string Name { get; set; }
+    public int NumberOfSeats  { get; set; }
+    public int NumberOfLoges  { get; set; }
+    public bool Indoors  { get; set; }
 }
