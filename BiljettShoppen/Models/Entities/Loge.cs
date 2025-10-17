e4343bfc239cf038b5f34b73e1e71e8bcd2382bf
camtil <camilla.tillander@outlook.com>
2025-10-17 10:16:23 +0200

    loge

---
+++ BiljettShoppen/Models/Entities/Loge.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Entities
+{
+    internal class Loge
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LogeNumber { get; set; }
+        public int NumberOfPeople { get; set; }
+        public decimal Price { get; set; }
+    }
+
+}
