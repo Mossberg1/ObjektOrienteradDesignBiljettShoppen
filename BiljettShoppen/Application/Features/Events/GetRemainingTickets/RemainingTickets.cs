515eb6de554c17c34d79839a507761defc3b1d06
Kasper Nordlinder <h24kasno@du.se>
2025-10-21 11:51:16 +0200

    GetRemainingTickets

---
+++ BiljettShoppen/Application/Features/Events/GetRemainingTickets/RemainingTickets.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Features.Events.GetRemainingTickets
+{
+    public class RemainingTickets
+    {
+        public int EventId { get; set; }
+        public string EventName { get; set; }
+        public int TicketsRemaining { get; set; }
+    }
+}
---
+++ BiljettShoppen/Application/Features/Events/GetRemainingTickets/RemainingTicketsHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Features.Events.GetRemainingTickets
+{
+    public class RemainingTicketsHandler : IRequestHandler<GetRemainingTicketsQuery, RemainingTickets>
+    {
+        private readonly IApplicationDbContext _dbContext;
+
+        public RemainingTicketsHandler(IApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<RemainingTickets> Handle(GetRemainingTicketsQuery request, CancellationToken cancellationToken)
+        {
+            var theEvent = await _dbContext.Events.AsNoTracking().FirstOrDefaultAsync(e => e.Id == request.EventId, cancellationToken);
+
+            if (theEvent == null)
+            {
+                throw new Exception($"Event with Id {request.EventId} not found.");
+            }
+
+            var totalCapacity = theEvent.NumberOfSeatsToSell + theEvent.NumberOfLogesToSell;
+
+            var soldTickets = await _dbContext.Tickets.CountAsync(t => t.EventId == request.EventId && t.BookingId != null, cancellationToken);
+
+            var remainingTickets = totalCapacity - soldTickets;
+
+            return new RemainingTickets
+            {
+                EventId = theEvent.Id,
+                EventName = theEvent.Name,
+                TicketsRemaining = remainingTickets
+            };
+        }
+    }
+}
