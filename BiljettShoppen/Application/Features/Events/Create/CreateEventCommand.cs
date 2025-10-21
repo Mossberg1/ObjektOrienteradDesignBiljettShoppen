7387183668701b25fa5f9ceab66442408dfcb9ff
William Mossberg <william.mossberg@outlook.com>
2025-10-21 22:09:28 +0200

    Mergat Alex kod

---
+++ BiljettShoppen/Application/Features/Events/Create/CreateEventCommand.cs
@@ -0,0 +1,22 @@
+using MediatR;
+using Models.Entities;
+using Models.Enums;
+
+namespace Application.Features.Events.Create;
+
+
+public record CreateEventCommand(
+    string Name,
+    DateOnly Date,
+    TimeOnly StartTime,
+    TimeOnly EndTime,
+    DateTime ReleaseTicketsDate,
+    int NumberOfSeatsToSell,
+    int NumberOfLogesToSell,
+    decimal Price,
+    decimal Cost,
+    EventType Type,
+    bool IsFamilyFriendly,
+    int ArenaId,
+    int SeatLayoutId
+) : IRequest<Event>;
