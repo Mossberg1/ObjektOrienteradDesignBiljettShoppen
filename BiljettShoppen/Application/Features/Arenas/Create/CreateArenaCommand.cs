dd5d20b664619d810530c973be8367ecb3d9cf52
h24noale <alexandernorberg22@gmail.com>
2025-10-21 09:13:21 +0200

    la till CreateArenaCommand och CreateArenaHandler för att skapa ny arena

---
+++ BiljettShoppen/Application/Features/Arenas/Create/CreateArenaCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Models.Entities;
+
+
+namespace Application.Features.Arenas.Create;
+public record CreateArenaCommand( // Data container för att skapa en ny arena.
+    string Address,
+    string Name,
+    int NumberOfSeats,
+    int NumberOfLoges,
+    bool Indoors
+) : IRequest<Arena>; // Talar om för MediatR att detta är en request för att skapa en ny arena.
+
