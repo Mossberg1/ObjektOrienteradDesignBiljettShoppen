47a07b8da6f7f7630ba6e3f38eafbbf487f9ef39
h24noale <alexandernorberg22@gmail.com>
2025-10-23 11:15:53 +0200

    La till CreateBookingHandler och CreateBookingCommand

---
+++ BiljettShoppen/Application/Features/Booking/Create/CreateBookingHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Interfaces;
+using DataAccess.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Models.Entities;
+
+namespace Application.Features.Booking.Create
+{
+    public class CreateBookingHandler : IRequestHandler<CreateBookingCommand, Models.Entities.Booking>
+    {
+        private readonly IApplicationDbContext _dbContext;
+        private readonly IBookingTimer _bookingTimer;
+
+        public CreateBookingHandler(IApplicationDbContext dbContext, IBookingTimer bookingTimer)
+        {
+            _dbContext = dbContext;
+            _bookingTimer = bookingTimer;
+        }
+
+        public async Task<Models.Entities.Booking> Handle(CreateBookingCommand request, CancellationToken cancellationToken)
+        {
+            var booking = new Models.Entities.Booking
+            {
+                TotalPrice = request.TotalPrice,
+                IsPaid = request.IsPaid,
+                PaymentNavigation = request.PaymentNavigation,
+                TicketsNavigation = request.TicketsNavigation,
+            };
+
+            _bookingTimer.AddBooking(booking);
+
+            _dbContext.Bookings.Add(booking);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return booking;
+        }
+    }
+}
