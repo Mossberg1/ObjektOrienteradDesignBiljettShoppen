6caf27f5803063e2ec113bfe1c5a0012d40ebd73
h24noale <alexandernorberg22@gmail.com>
2025-10-27 11:41:02 +0100

    Försök 2 att lägga till ListArenasQuery och ListArenasHandler

---
+++ BiljettShoppen/Application/Features/Arenas/ListArenas/ListArenasHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using DataAccess.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Models.Entities;
+
+namespace Application.Features.Arenas
+{
+    public class ListArenasHandler : IRequestHandler<ListArenasQuery, List<Arena>>
+    {
+        private readonly IApplicationDbContext _dbContext;
+
+        public ListArenasHandler(IApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Arena>> Handle(ListArenasQuery request, CancellationToken cancellationToken)
+        {
+            return await _dbContext.Arenas
+                .AsNoTracking()
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
