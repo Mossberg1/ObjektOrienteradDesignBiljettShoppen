d3494b18cabbb4ad64d2cedb882f076ceca75b54
h24tober <h24tober@du.se>
2025-10-28 10:59:07 +0100

    Lade till delete funktion på event

---
+++ BiljettShoppen/Application/Features/Events/Delete/DeleteEventHandler.cs
@@ -0,0 +1,29 @@
+﻿using DataAccess.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Features.Events.Delete;
+
+public class DeleteEventHandler:IRequestHandler<DeleteEventCommand, bool>
+{
+    private readonly IApplicationDbContext _dbContext;
+    
+    public DeleteEventHandler(IApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<bool> Handle(DeleteEventCommand request, CancellationToken cancellationToken)
+    {
+        var eventEntity = await _dbContext.Events
+            .FirstOrDefaultAsync(e => e.Id == request.EventId, cancellationToken);
+
+        if (eventEntity == null)
+            return false;
+
+        _dbContext.Events.Remove(eventEntity);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+
+        return true;
+    }
+}
