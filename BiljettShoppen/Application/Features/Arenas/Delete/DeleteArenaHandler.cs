c4f0c218531dec9c31b926c5423c6af352b93380
h24noale <alexandernorberg22@gmail.com>
2025-10-28 11:27:11 +0100

    NU ÄR DEN SPARAD (Delete Arena)

---
+++ BiljettShoppen/Application/Features/Arenas/Delete/DeleteArenaHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Models.Entities;
+
+namespace Application.Features.Arenas.Delete
+{
+    public class DeleteArenaHandler : IRequestHandler<DeleteArenaCommand, bool>
+    {
+        private readonly IApplicationDbContext _dbContext;
+
+        public DeleteArenaHandler(IApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> Handle(DeleteArenaCommand request, CancellationToken cancellationToken)
+        {
+            
+            var arena = await _dbContext.Arenas
+                .FirstOrDefaultAsync(a => a.Id == request.ArenaId, cancellationToken);
+
+            if (arena == null)
+                throw new KeyNotFoundException($"Arena with ID {request.ArenaId} not found.");
+            
+            _dbContext.Arenas.Remove(arena);
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return true;
+        }
+    }
+}
