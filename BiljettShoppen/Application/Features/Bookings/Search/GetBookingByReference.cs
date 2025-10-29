73b9c86792db1d5c742e1e1b810de79beaba91c2
h24tober <h24tober@du.se>
2025-10-29 11:43:06 +0100

    Lade till funktion för att söka på bokning

---
+++ BiljettShoppen/Application/Features/Bookings/Search/GetBookingByReference.cs
@@ -0,0 +1,29 @@
+﻿using DataAccess.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Models.Entities;
+
+namespace Application.Features.Bookings.Search;
+
+public class GetBookingByReferenceHandler : IRequestHandler<GetBookingByReferenceQuery, Booking?>
+{
+    private readonly IApplicationDbContext _dbContext;
+
+    public GetBookingByReferenceHandler(IApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<Booking?> Handle(GetBookingByReferenceQuery request, CancellationToken cancellationToken)
+    {
+        var booking = await _dbContext.Bookings
+            .Include(b => b.TicketsNavigation)
+            .ThenInclude(t => t.BookableSpaceNavigation)
+            .Include(b => b.TicketsNavigation)
+            .ThenInclude(t => t.EventNavigation)
+            .Include(b => b.PaymentNavigation)
+            .FirstOrDefaultAsync(b => b.ReferenceNumber == request.ReferenceNumber, cancellationToken);
+
+        return booking;
+    }
+}
---
+++ BiljettShoppen/Application/Features/Bookings/Search/GetBookingByReferenceQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Models.Entities;
+
+namespace Application.Features.Bookings.Search;
+
+
+public record GetBookingByReferenceQuery(
+    string ReferenceNumber
+) : IRequest<Booking?>;
+
