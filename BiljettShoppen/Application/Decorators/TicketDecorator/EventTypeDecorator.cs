72fb7716e3c276716f0cd9db1e72b78afa34e5b1
William Mossberg <136148492+Mossberg1@users.noreply.github.com>
2025-11-04 11:03:27 +0100

    fixat prisberäkningar

---
+++ BiljettShoppen/Application/Decorators/TicketDecorator/EventTypeDecorator.cs
@@ -0,0 +1,35 @@
+﻿using Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Decorators.TicketDecorator
+{
+    public class EventTypeDecorator : TicketDecorator
+    {
+        private readonly EventType _type;
+
+        public EventTypeDecorator(ITicketComponent wrappedTicket, EventType type) : base(wrappedTicket) 
+        {
+            _type = type;
+        }
+
+        public override decimal GetPrice()
+        {
+            decimal multiplier = _type switch 
+            {
+                EventType.Concert => 1.5m,
+                EventType.Sports => 1.2m,
+                EventType.Theater => 0.9m,
+                EventType.ComedyShow => 1.1m,
+                _ => 1.0m
+            };
+
+            return base.GetPrice() * multiplier;
+        }
+
+        public override string GetDescription() => $"{base.GetDescription()}, Evenemangstyp: {_type}";
+    }
+}
